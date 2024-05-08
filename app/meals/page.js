9deb5dfe726199ff4d9c3e810b6500b4e9d3ed9d
Jake Licmo <jlicmo2@academic.rrc.ca>
2024-05-08 11:30:40 -0500

    Not Found, Error, Meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,37 @@
-export default function MealsPage(){
-    return(
-        <main>
-        <h1 style={{ color: 'white', textAlign: 'center' }}>
-          Meals page.
+import MealsGrid from "../components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import MealsLoadingPage from "./loading-out";
+
+async function Meals(){
+  const meals = await getMeals();
+
+  return <MealsGrid meals = {meals}/>
+}
+
+
+export default function MealsPage() {
+
+
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
         </h1>
-        </main>
-    );
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals/>
+        </Suspense>
+      </main>
+    </>
+  );
 }
