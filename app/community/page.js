d1e976751e4eb950b3141bb05149894ed36e1430
Jake Licmo <jlicmo2@academic.rrc.ca>
2024-05-08 08:55:30 -0500

    First commit

---
+++ app/community/page.js
@@ -0,0 +1,10 @@
+export default function Community() {
+    return (
+      <main>
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+          This is the community page.
+        </h1>
+      </main>
+    );
+  }
+  
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+export default function ShareMealsPage(){
+    return(
+        <main>
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+          Share meals page.
+        </h1>
+        </main>
+    );
+}
